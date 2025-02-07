726ead868b713377d97b47fbf870456de026b34b
helderargomes <helderargomes@gmail.com>
2025-02-07 08:17:02 -0300

    Redirecting to login when authorization fails

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -2,10 +2,11 @@ import Navbar from "components/Navbar";
 import Home from "pages/Home";
 import MovieReviews from "pages/MovieReviews";
 import Movies from "pages/Movies";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "utils/history";
 
 const Routes = () => (
-  <BrowserRouter>
+  <Router history={ history}>
     <Navbar />
     <Switch>
       <Route path="/" exact>
@@ -18,6 +19,6 @@ const Routes = () => (
         <MovieReviews />
       </Route>
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 export default Routes;
