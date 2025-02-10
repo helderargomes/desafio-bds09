a18e449d4c289fa2ac53a16ff9d68aaacc0155c3
helderargomes <helderargomes@gmail.com>
2025-02-09 21:43:30 -0300

    Review form input and validation

--- frontweb/src/pages/MovieReviews/ReviewForm/index.tsx
+++ frontweb/src/pages/MovieReviews/ReviewForm/index.tsx
@@ -1,16 +1,38 @@
+import { useForm } from "react-hook-form";
 import "./styles.css";
 
+type ReviewData = {
+  review: string;
+}
+
 const ReviewForm = () => {
+  
+  const {register, handleSubmit, formState: { errors } } = useForm<ReviewData>();
+
+  const onSubmit = (reviewData: ReviewData) => {
+    console.log(reviewData);
+  }
+
   return (
+
+
     <div className="base-card comment-card">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3">
           <input
+          {...register("review", {
+            required: 'Campo obrigatório'
+          })}
             type="text"
-            className="form-control base-input"
+            className={`form-control base-input ${
+              errors.review ? "is-invalid" : ""
+            }`}
             placeholder="Deixe sua avaliação aqui"
-            name="comment"
+            name="review"
           />
+        </div>
+        <div className="invalid-feedback d-block">
+            {errors.review?.message}
         </div>
         <div className="comment-submit">
           <button className="bg-primary btn">Salvar Avaliação</button>
